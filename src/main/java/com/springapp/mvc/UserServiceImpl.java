db52df117b9a78372e88426f3f67935f0a8b8a6d
WojtekSasiela <wojtek.sasiela@gmail.com>
2015-11-18 00:30:57 +0100

    checking work JPQL ( Java Persistence Query Language ) query in UserServiceImpl

--- src/main/java/com/springapp/mvc/UserServiceImpl.java
+++ src/main/java/com/springapp/mvc/UserServiceImpl.java
@@ -6,22 +6,39 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.List;
+
 @Service
 public class UserServiceImpl implements UserService{
 
     @Autowired private UserRepository userRepository;
 
+    @PersistenceContext
+    EntityManager entityManager;
+
     @Transactional
     @Override
     public String getUserNameAndSurname(Long id) {
 
+        String name = "", surname = "";
         ApplicationContext context = new ClassPathXmlApplicationContext("springs.xml");
         User user = (User) context.getBean("User");
         userRepository.saveAndFlush(user);
+        user = null;
 
+        // 1st Case to gets name and surname  from database
+        Query query = entityManager.createQuery("select a from account a");
+        List<User> users = query.getResultList();
+
+        //2nd case to get name and surname from database
 //      User user = userRepository.findOne(id);
-        String name = user.getFirstName();
-        String surname = user.getLastName();
+//      name = user.getFirstName();
+//      surname = user.getLastName();
+        name = users.get(0).getFirstName();
+        surname = users.get(0).getLastName();
         return name + " " + surname;
     }
 }
